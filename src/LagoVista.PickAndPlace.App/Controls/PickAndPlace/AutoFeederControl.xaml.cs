9de25c5ece494d5a7e9083eb0adfbf378d568f06
Kevin D. Wolf <kevinw@software-logistics.com>
2025-01-01 19:32:54 -0500

    start work on feeder management

---
+++ src/LagoVista.PickAndPlace.App/Controls/PickAndPlace/AutoFeederControl.xaml.cs
@@ -0,0 +1,30 @@
+﻿using LagoVista.PickAndPlace.Interfaces.ViewModels.PickAndPlace;
+using LagoVista.XPlat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LagoVista.PickAndPlace.App.Controls.PickAndPlace
+{
+    /// <summary>
+    /// Interaction logic for AutoFeederControl.xaml
+    /// </summary>
+    public partial class AutoFeederControl : VMBoundUserControl<IPartsViewModel>
+    {
+        public AutoFeederControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
