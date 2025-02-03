37475e2b597d93d01eab271cf4f551cbaf98a29a
Kevin Wolf <kevinw@software-logistics.com>
2025-02-03 07:04:06 -0500

    server side support for creating job runs

--- src/LagoVista.Manufacturing.Rest/PnpJobController.cs
+++ src/LagoVista.Manufacturing.Rest/PnpJobController.cs
@@ -9,8 +9,8 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
-using System.Security.Cryptography;
-using RingCentral;
+using LagoVista.Core.Interfaces;
+using System;
 
 namespace LagoVista.Manufacturing.Rest.Controllers
 {
@@ -18,17 +18,19 @@ namespace LagoVista.Manufacturing.Rest.Controllers
     [Authorize]
     public class PickAndPlaceJobController : LagoVistaBaseController
     {
-        private readonly IPickAndPlaceJobManager _mgr;
+        private readonly IPickAndPlaceJobManager _pnpJobManager;
+        private readonly ISerialNumberManager _serialNumberManager;
 
-        public PickAndPlaceJobController(UserManager<AppUser> userManager, IAdminLogger logger, IPickAndPlaceJobManager mgr) : base(userManager, logger)
+        public PickAndPlaceJobController(UserManager<AppUser> userManager, ISerialNumberManager serialNumberManager, IAdminLogger logger, IPickAndPlaceJobManager pnpJobManager) : base(userManager, logger)
         {
-            _mgr = mgr;
+            _pnpJobManager = pnpJobManager ?? throw new ArgumentNullException(nameof(pnpJobManager));
+            _serialNumberManager = serialNumberManager ?? throw new ArgumentNullException(nameof(serialNumberManager));
         }
 
         [HttpGet("/api/mfg/pnpjob/{id}")]
         public async Task<DetailResponse<PickAndPlaceJob>> GetPickAndPlaceJob(string id)
         {
-            return DetailResponse<PickAndPlaceJob>.Create(await _mgr.GetPickAndPlaceJobAsync(id, OrgEntityHeader, UserEntityHeader));
+            return DetailResponse<PickAndPlaceJob>.Create(await _pnpJobManager.GetPickAndPlaceJobAsync(id, OrgEntityHeader, UserEntityHeader));
         }
 
         [HttpGet("/api/mfg/pnpjob/factory")]
@@ -43,74 +45,77 @@ namespace LagoVista.Manufacturing.Rest.Controllers
         [HttpDelete("/api/mfg/pnpjob/{id}")]
         public async Task<InvokeResult> DeletePickAndPlaceJob(string id)
         {
-            return await _mgr.DeletePickAndPlaceJobAsync(id, OrgEntityHeader, UserEntityHeader);
+            return await _pnpJobManager.DeletePickAndPlaceJobAsync(id, OrgEntityHeader, UserEntityHeader);
         }
 
         [HttpPost("/api/mfg/pnpjob")]
         public Task<InvokeResult> AddPickAndPlaceJobAsync([FromBody] PickAndPlaceJob PickAndPlaceJob)
         {
-            return _mgr.AddPickAndPlaceJobAsync(PickAndPlaceJob, OrgEntityHeader, UserEntityHeader);
+            return _pnpJobManager.AddPickAndPlaceJobAsync(PickAndPlaceJob, OrgEntityHeader, UserEntityHeader);
         }
 
         [HttpPut("/api/mfg/pnpjob")]
         public Task<InvokeResult> UpdatePickAndPlaceJob([FromBody] PickAndPlaceJob PickAndPlaceJob)
         {
             SetUpdatedProperties(PickAndPlaceJob);
-            return _mgr.UpdatePickAndPlaceJobAsync(PickAndPlaceJob, OrgEntityHeader, UserEntityHeader);
+            return _pnpJobManager.UpdatePickAndPlaceJobAsync(PickAndPlaceJob, OrgEntityHeader, UserEntityHeader);
         }
 
         [HttpGet("/api/mfg/pnpjobs")]
         public Task<ListResponse<PickAndPlaceJobSummary>> GetPnPJobs()
         {
-            return _mgr.GetPickAndPlaceJobSummariesAsync(GetListRequestFromHeader(), OrgEntityHeader, UserEntityHeader);
+            return _pnpJobManager.GetPickAndPlaceJobSummariesAsync(GetListRequestFromHeader(), OrgEntityHeader, UserEntityHeader);
         }
 
         [HttpGet("/api/mfg/pnpjob/run/{id}")]
         public async Task<DetailResponse<PickAndPlaceJobRun>> GetPickAndPlaceJobRun(string id)
         {
-            return DetailResponse<PickAndPlaceJobRun>.Create(await _mgr.GetPickAndPlaceJobRunAsync(id, OrgEntityHeader, UserEntityHeader));
+            return DetailResponse<PickAndPlaceJobRun>.Create(await _pnpJobManager.GetPickAndPlaceJobRunAsync(id, OrgEntityHeader, UserEntityHeader));
         }
 
         [HttpGet("/api/mfg/pnpjob/{jobid}/run/factory")]
         public async Task<DetailResponse<PickAndPlaceJobRun>> CreatePickAndPlaceJobRun(string jobid)
         {
-            var job = await _mgr.GetPickAndPlaceJobAsync(jobid, OrgEntityHeader, UserEntityHeader);
+            var job = await _pnpJobManager.GetPickAndPlaceJobAsync(jobid, OrgEntityHeader, UserEntityHeader);
 
             var form = DetailResponse<PickAndPlaceJobRun>.Create();
+            form.Model.SerialNumber = await _serialNumberManager.GenerateSerialNumber(OrgEntityHeader.Id, job.Key);
+            form.Model.Name = $"{job.Name} ({form.Model.SerialNumber})";
+            form.Model.Key = $"{job.Key}{form.Model.SerialNumber}";
             form.Model.Job = job.ToEntityHeader();
             form.Model.RanBy = UserEntityHeader;
             form.Model.Cost = job.Cost;
             form.Model.Extended = job.Extended;
             SetAuditProperties(form.Model);
             SetOwnedProperties(form.Model);
-            await _mgr.AddPickAndPlaceJobRunAsync(form.Model, OrgEntityHeader, UserEntityHeader);
+            await _pnpJobManager.AddPickAndPlaceJobRunAsync(form.Model, OrgEntityHeader, UserEntityHeader);
             return form;
         }
 
         [HttpPost("/api/mfg/pnpjob/run")]
         public Task<InvokeResult> AddPickAndPlaceJobRunAsync([FromBody] PickAndPlaceJobRun run)
         {
-            return _mgr.AddPickAndPlaceJobRunAsync(run, OrgEntityHeader, UserEntityHeader);
+            return _pnpJobManager.AddPickAndPlaceJobRunAsync(run, OrgEntityHeader, UserEntityHeader);
         }
 
         [HttpPut("/api/mfg/pnpjob/run")]
         public Task<InvokeResult> UpdatePickAndPlaceJobRun([FromBody] PickAndPlaceJobRun run)
         {
             SetUpdatedProperties(run);
-            return _mgr.UpdatePickAndPlaceJobRunAsync(run, OrgEntityHeader, UserEntityHeader);
+            return _pnpJobManager.UpdatePickAndPlaceJobRunAsync(run, OrgEntityHeader, UserEntityHeader);
         }
 
         [HttpPut("/api/mfg/pmpjob/run/{id}/placement")]
         public Task<InvokeResult> UpdatePickAndPlaceJobRun(string id, [FromBody] PickAndPlaceJobRunPlacement placement)
         {
-            return _mgr.UpdatePickAndPlaceJobRunPlacementAsync(id, placement, OrgEntityHeader, UserEntityHeader);
+            return _pnpJobManager.UpdatePickAndPlaceJobRunPlacementAsync(id, placement, OrgEntityHeader, UserEntityHeader);
         }
 
 
         [HttpGet("/api/mfg/pnpjob/runs")]
         public Task<ListResponse<PickAndPlaceJobSummary>> GetPnPJobRunss()
         {
-            return _mgr.GetPickAndPlaceJobSummariesAsync(GetListRequestFromHeader(), OrgEntityHeader, UserEntityHeader);
+            return _pnpJobManager.GetPickAndPlaceJobSummariesAsync(GetListRequestFromHeader(), OrgEntityHeader, UserEntityHeader);
         }
     }
 }
