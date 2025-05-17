5d0c0797f15190a6806d1355767bb11be1801379
Kevin D. Wolf <kevinw@software-logistics.com>
2025-05-17 14:58:58 -0400

    rendering in WPF app.

--- src/LagoVista.PickAndPlace.App/Controls/Machine/MachineRender.xaml.cs
+++ src/LagoVista.PickAndPlace.App/Controls/Machine/MachineRender.xaml.cs
@@ -1,11 +1,12 @@
 ﻿using HelixToolkit.Wpf;
-using LagoVista.PickAndPlace.App.ViewModels;
+using LagoVista.Core.Models.Drawing;
+using LagoVista.PickAndPlace.App.Controls.PcbFab;
 using LagoVista.PickAndPlace.Interfaces.ViewModels.PickAndPlace;
 using LagoVista.XPlat;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Media3D;
 
@@ -20,28 +21,39 @@ namespace LagoVista.PickAndPlace.App.Controls
         {
             InitializeComponent();
             DataContextChanged += MachineRender_DataContextChanged;
+            this.Loaded += MachineRender_Loaded;
+        }
+
+        private void MachineRender_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (ViewModel != null)
+            {
+                ViewModel.PropertyChanged += ViewModel_PropertyChanged;
+                ViewModel.AutoFeederViewModel.PropertyChanged += ViewModel_PropertyChanged;
+                ViewModel.StripFeederViewModel.PropertyChanged += ViewModel_PropertyChanged;
+            }
         }
 
         private void ViewModel_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
         {
-            if(e.PropertyName == nameof(IPartsViewModel.Machine))
+            if (e.PropertyName == nameof(IPartsViewModel.Machine))
             {
-                if(ViewModel.Machine != null)
+                if (ViewModel.Machine != null)
                     Machine = ViewModel.Machine.Settings;
             }
 
-            if(e.PropertyName == nameof(IStripFeederViewModel.Feeders))
+            if (e.PropertyName == nameof(IStripFeederViewModel.Feeders))
             {
                 StripFeeders = ViewModel.StripFeederViewModel.Feeders;
             }
 
-            if(e.PropertyName == nameof(IAutoFeederViewModel.Feeders))
+            if (e.PropertyName == nameof(IAutoFeederViewModel.Feeders))
             {
                 AutoFeeders = ViewModel.AutoFeederViewModel.Feeders;
             }
         }
 
-        Material copperMaterial = MaterialHelper.CreateMaterial(Color.FromRgb(0xb8, 0x73, 0x33));
+        Material copperMaterial = MaterialHelper.CreateMaterial(System.Windows.Media.Color.FromRgb(0xb8, 0x73, 0x33));
         Material whiteMaterial = MaterialHelper.CreateMaterial(Colors.White);
         Material redMaterial = MaterialHelper.CreateMaterial(Colors.Red);
         Material brownMaterial = MaterialHelper.CreateMaterial(Colors.Brown);
@@ -64,9 +76,11 @@ namespace LagoVista.PickAndPlace.App.Controls
         }
 
         LagoVista.Manufacturing.Models.Machine _machine;
-        public LagoVista.Manufacturing.Models.Machine Machine 
-        {   get => _machine;
-            set {
+        public LagoVista.Manufacturing.Models.Machine Machine
+        {
+            get => _machine;
+            set
+            {
                 _machine = value;
                 if (_machine != null)
                 {
@@ -137,11 +151,11 @@ namespace LagoVista.PickAndPlace.App.Controls
             modelGroup.Children.Add(boxModel);
 
             var railMeshBuilder = new MeshBuilder(false, false);
-            railMeshBuilder.AddBox(new Rect3D(3, 0, 70,14, machine.FrameSize.Y, 5));
+            railMeshBuilder.AddBox(new Rect3D(3, 0, 70, 14, machine.FrameSize.Y, 5));
             railMeshBuilder.AddBox(new Rect3D(machine.FrameSize.X - 17, 0, 70, 14, machine.FrameSize.Y, 5));
             var railModel = new GeometryModel3D() { Geometry = railMeshBuilder.ToMesh(true), Material = whiteMaterial };
             modelGroup.Children.Add(railModel);
-            
+
 
             foreach (var feederRail in machine.FeederRails)
             {
@@ -149,7 +163,7 @@ namespace LagoVista.PickAndPlace.App.Controls
                 var rect = new Rect3D(0, feederRail.FirstFeederOrigin.Y, -43, machine.FrameSize.X, 20, 20);
                 feederMeshBuilder.AddBox(rect);
                 var feederRailModel = new GeometryModel3D() { Geometry = feederMeshBuilder.ToMesh(true), Material = grayMaterial };
-                modelGroup.Children.Add(feederRailModel);                
+                modelGroup.Children.Add(feederRailModel);
             }
 
             FrameLayer.Content = modelGroup;
@@ -161,7 +175,7 @@ namespace LagoVista.PickAndPlace.App.Controls
             foreach (var plate in machine.StagingPlates)
             {
                 var topWireMeshBuilder = new MeshBuilder(false, false);
-                var boxRect = new Rect3D(plate.Origin.X, plate.Origin.Y, 0 , plate.Size.X, plate.Size.Y, 3);
+                var boxRect = new Rect3D(plate.Origin.X, plate.Origin.Y, 0, plate.Size.X, plate.Size.Y, 3);
                 topWireMeshBuilder.AddBox(boxRect);
 
                 var r = Convert.ToByte(plate.Color.Substring(1, 2), 16);
@@ -169,43 +183,62 @@ namespace LagoVista.PickAndPlace.App.Controls
                 var b = Convert.ToByte(plate.Color.Substring(5, 2), 16);
                 //                plate.Color = 
 
-                var material = MaterialHelper.CreateMaterial(Color.FromRgb(r,g,b));
+                var material = MaterialHelper.CreateMaterial(System.Windows.Media.Color.FromRgb(r, g, b));
                 var boxModel = new GeometryModel3D() { Geometry = topWireMeshBuilder.ToMesh(true), Material = material };
 
                 var holeMeshBuilder = new MeshBuilder(false, false);
                 var maskMeshBuilder = new MeshBuilder(false, false);
 
-
-                
-                for (double x = plate.HoleSpacing / 2; x <= plate.Size.X - 16; x += (plate.HoleSpacing))
+                for (double plateX = 0; plateX < plate.HoleSpacing * plate.LastUsableColumn / 2; plateX += (plate.HoleSpacing))
                 {
                     var idx = 0;
-                    for (double y = plate.HoleSpacing / 2; y < plate.Size.Y; y += (plate.HoleSpacing / 2))
+                    for (double plateY = 0; plateY < plate.Size.Y; plateY += (plate.HoleSpacing / 2))
                     {
-                        var xOFfset = (idx++ % 2 == 0) ? x : x + 15;
-                        maskMeshBuilder.AddCylinder(new Point3D(xOFfset, y + plate.Origin.Y, -0.025), new Point3D(xOFfset, y + plate.Origin.Y, 3.05), 3.25);
-                        holeMeshBuilder.AddCylinder(new Point3D(xOFfset, y + plate.Origin.Y, -0.05), new Point3D(xOFfset, y + plate.Origin.Y, 3.1), 1.5);
+                        var xOFfset = (idx++ % 2 == 0) ? plateX : plateX + 15;
+
+                        var x = xOFfset + plate.FirstHole.X;
+                        var y = plateY + plate.FirstHole.Y + plate.Origin.Y;
+
+                        maskMeshBuilder.AddCylinder(new Point3D(x, y, -0.025), new Point3D(x, y, 3.05), 3.25);
+                        holeMeshBuilder.AddCylinder(new Point3D(x, y, -0.05), new Point3D(x, y, 3.1), 1.5);
                     }
-                }             
+                }
 
                 modelGroup.Children.Add(new GeometryModel3D() { Geometry = holeMeshBuilder.ToMesh(true), Material = whiteMaterial });
                 modelGroup.Children.Add(new GeometryModel3D() { Geometry = maskMeshBuilder.ToMesh(true), Material = copperMaterial });
                 modelGroup.Children.Add(boxModel);
+
+                var feederRender = new ThreeD.StripFeeder();
+
+                if (ViewModel.StripFeederViewModel.Feeders != null)
+                {
+                    foreach (var feeder in ViewModel.StripFeederViewModel.Feeders)
+                    {
+                        var keyRow = feeder.ReferenceHoleRow.Key;
+                        var keyCol = feeder.ReferenceHoleColumn.Key;
+                        var colIndex = int.Parse(keyCol) - 1;
+                        var rowIndex = keyRow[0] - 'A' - 1;
+
+                        var xOffset = (colIndex * 15 + plate.Origin.X + plate.FirstHole.X) - feeder.MountingHoleOffset.X;
+                        var yOffset = (rowIndex * 15 + plate.Origin.Y + plate.FirstHole.Y) - feeder.MountingHoleOffset.Y;
+
+                        feederRender.Render3DModel(modelGroup, feeder, new Point2D<double>(xOffset, yOffset), feeder.Orientation.Key == "horizontal" ? 0 : -90);
+                    }
+                }
             }
 
             StagingPlatesLayer.Content = modelGroup;
-            //StagingPlatesLayer.Transform = new TranslateTransform3D(scrapX, scrapY, 0);            
         }
 
         private async void Refresh_Click(object sender, RoutedEventArgs e)
         {
-            if(Machine != null)
+            if (Machine != null)
             {
                 await ViewModel.ReloadedAsync();
                 RenderFrame(Machine);
                 RenderStagingPlates(Machine);
             }
-        }       
+        }
 
         private void ResetZoom_Click(object sender, RoutedEventArgs e)
         {
@@ -219,7 +252,109 @@ namespace LagoVista.PickAndPlace.App.Controls
             var z = 7 * factor;
 
             Camera.Position = new Point3D(x, y, z);
-            var dir = Camera.LookDirection;         
+            var dir = Camera.LookDirection;
+
+        }
+
+  
+
+
+        ImageModes _imageMode = ImageModes.Front;
+
+        private void ChangeView_Handler(object sender, RoutedEventArgs e)
+        {
+            var btn = sender as Button;
+            switch (btn.Tag.ToString())
+            {
+                case "Top":
+                    {
+                        _imageMode = ImageModes.Top;
+                        ShowTopView();
+                    }
+                    break;
+                case "Left":
+                    {
+                        _imageMode = ImageModes.Side;
+                        ShowLeftView();
+                    }
+                    break;
+                case "Front":
+                    {
+                        _imageMode = ImageModes.Front;
+                        ShowFrontView();
+                    }
+                    break;
+            }
+        }
+
+        private void ShowLeftView()
+        {
+            //double min = ViewModel.GCodeFileManager.HasValidFile ? ViewModel.GCodeFileManager.Min.Y : 0;
+            //double max = ViewModel.Machine.Settings.WorkAreaSize.Y;
+
+            //if (ViewModel.PCBManager.HasBoard)
+            //    max = ViewModel.PCBManager.Board.Height;
+
+            //if (ViewModel.GCodeFileManager.HasValidFile)
+            //    max = ViewModel.GCodeFileManager.Max.Y;
+
+            //var factor = 18.0;
+
+            //var delta = (max - min);
+            //var y = (delta / 2) + min;
+            //var x = -12 * factor;
+            //var z = 7 * factor;
+
+            //Camera.Position = new Point3D(x, y, z);
+            //Camera.LookDirection = new Vector3D(4, 0.0001, -1.7);
+
+            //CameraPosition.Text = $"{Camera.Position.X},{Camera.Position.Y},{Camera.Position.Z}";
+
+        }
+
+        private void ShowTopView()
+        {
+            double minX = -20;
+            double maxX = _machine.FrameSize.X;
+
+            var minY = -20;
+            var maxY = _machine.FrameSize.Y;
+
+
+            var deltaX = maxX - minX;
+            var deltaY = maxY - minY;
+
+            var x = deltaX / 2 + minX;
+            var y = deltaY / 2 + minY;
+
+            Camera.Position = new Point3D(x, y, 400);
+            Camera.LookDirection = new Vector3D(0, 0.0001, -1);
+
+            _imageMode = ImageModes.Top;
+        }
+
+        private void ShowFrontView()
+        {
+            //double min = ViewModel.GCodeFileManager.HasValidFile ? ViewModel.GCodeFileManager.Min.X : 0;
+            //double max = ViewModel.Machine.Settings.WorkAreaSize.X;
+
+            //if (ViewModel.PCBManager.HasBoard)
+            //    max = ViewModel.PCBManager.Board.Width;
+
+            //if (ViewModel.GCodeFileManager.HasValidFile)
+            //    max = ViewModel.GCodeFileManager.Max.X;
+
+            //var factor = 18.0;
+
+            //var delta = (max - min);
+            //var x = (delta / 2) + min;
+            //var y = -12 * factor;
+            //var z = 7 * factor;
+
+            //Camera.Position = new Point3D(x, y, z);
+            //Camera.LookDirection = new Vector3D(0.0001, 4, -1.7);
+
+            //CameraPosition.Text = $"{Camera.Position.X},{Camera.Position.Y},{Camera.Position.Z}";
 
         }
     }
--- src/LagoVista.PickAndPlace.App/ThreeD/StripFeeder.cs
+++ src/LagoVista.PickAndPlace.App/ThreeD/StripFeeder.cs
@@ -1,25 +1,19 @@
 ﻿using HelixToolkit.Wpf;
-using LagoVista.Manufacturing.Models;
-using MSDMarkwort.Kicad.Parser.PcbNew;
-using PdfSharpCore.Drawing;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using LagoVista.Core.Models.Drawing;
 using System.Windows.Media.Media3D;
 
 namespace LagoVista.PickAndPlace.App.ThreeD
 {
     public class StripFeeder : DrawingBase
     {
-        public void Render3DModel(Model3DGroup modelGroup, LagoVista.Manufacturing.Models.StripFeeder model)
+        public void Render3DModel(Model3DGroup parentGroup, LagoVista.Manufacturing.Models.StripFeeder model, Point2D<double> origin = null, double? rotate = null)
         {
+            var feederModelGroup = new Model3DGroup();
             var baseMeshBuilder = new MeshBuilder(false, false);
             var baseRect = new Rect3D(0, 0, 0, model.Length, model.Width, 4);
             baseMeshBuilder.AddBox(baseRect);
             var boxModel = new GeometryModel3D() { Geometry = baseMeshBuilder.ToMesh(true), Material = BlackMaterial };
-            modelGroup.Children.Add(boxModel);
+            feederModelGroup.Children.Add(boxModel);
 
             for (var idx = 0; idx < model.RowCount; ++idx)
             {
@@ -32,10 +26,10 @@ namespace LagoVista.PickAndPlace.App.ThreeD
                 rowMeshBuilder.AddBox(side1Rect);
                 rowMeshBuilder.AddBox(side2Rect);
                 var rowModel = new GeometryModel3D() { Geometry = rowMeshBuilder.ToMesh(true), Material = BlueMaterial };
-                modelGroup.Children.Add(rowModel);
+                feederModelGroup.Children.Add(rowModel);
 
                 var row = model.Rows[idx];
-                if(row.Component != null && row.Component.Value?.ComponentPackage != null)
+                if (row.Component != null && row.Component.Value?.ComponentPackage != null)
                 {
                     var package = row.Component.Value.ComponentPackage.Value;
                     var tapeY = rowBottomY + (model.RowWidth / 2) - (package.TapeWidth.Value / 2) - 1;
@@ -45,25 +39,40 @@ namespace LagoVista.PickAndPlace.App.ThreeD
                     var tapeRect = new Rect3D(0, tapeY, model.Height - 1, model.Length, tapeWidth.Value, 0.2);
                     tapeMeshBuilder.AddBox(tapeRect);
                     var tapeModel = new GeometryModel3D() { Geometry = tapeMeshBuilder.ToMesh(true), Material = WhiteMaterial };
-                    modelGroup.Children.Add(tapeModel);
+                    feederModelGroup.Children.Add(tapeModel);
                     var componentCount = model.Length / package.SpacingX.Value;
                     for (var cmptIdx = 0; cmptIdx < componentCount - 1; ++cmptIdx)
                     {
                         var y = (package.CenterY - package.Width / 2).Value;
 
                         var componentModel = Component.Render3DModel(row.Component.Value.ComponentPackage.Value);
-                        componentModel.Transform = new TranslateTransform3D(cmptIdx * package.SpacingX.Value + 3 , tapeY + y , ((model.Height - 1) - package.Height) + 0.3);
-                        modelGroup.Children.Add(componentModel);
+                        componentModel.Transform = new TranslateTransform3D(cmptIdx * package.SpacingX.Value + 3, tapeY + y, ((model.Height - 1) - package.Height) + 0.3);
+                        feederModelGroup.Children.Add(componentModel);
                     }
 
-                    for(var holeIdx = 2; holeIdx < model.Length; holeIdx += 4)
+                    for (var holeIdx = 2; holeIdx < model.Length; holeIdx += 4)
                     {
                         var holeMeshBuilder = new MeshBuilder(false, false);
-                       holeMeshBuilder.AddCylinder(new Point3D(holeIdx, tapeY + 1.75 , model.Height - 0.8), new Point3D(holeIdx, tapeY + 1.75, model.Height - 0.7));
-                       modelGroup.Children.Add(new GeometryModel3D() { Geometry = holeMeshBuilder.ToMesh(true), Material = BlackMaterial });
+                        holeMeshBuilder.AddCylinder(new Point3D(holeIdx, tapeY + 1.75, model.Height - 0.8), new Point3D(holeIdx, tapeY + 1.75, model.Height - 0.7));
+                        feederModelGroup.Children.Add(new GeometryModel3D() { Geometry = holeMeshBuilder.ToMesh(true), Material = BlackMaterial });
                     }
                 }
-            }            
+            }
+
+            if (origin != null)
+            {
+                var grp = new Transform3DGroup();
+
+                if (rotate.HasValue)
+                    grp.Children.Add(new RotateTransform3D(new AxisAngleRotation3D(new Vector3D(0, 0, 1), rotate.Value),new Point3D(0,0,0)));
+
+                grp.Children.Add(new TranslateTransform3D(origin.X, origin.Y, 0));
+
+
+                feederModelGroup.Transform = grp;
+            }
+         
+            parentGroup.Children.Add(feederModelGroup);
         }
     }
 }
